1b8b4daed3bfe5dfe48538968385c4af628e19d2
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-04-24 18:30:11 +0300

    add links to pages to header

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import styles from "./MainHeader.module.scss";
 
+import { NavLink, useLocation } from "react-router-dom";
+
+const getStyle = ({ isActive }: { isActive: boolean }) =>
+  isActive ? styles["header--active"] : "";
+
+const getStyleAuth = (pathname: string) =>
+  ["/sign-in", "/sign-up"].includes(pathname) ? styles["header--active"] : "";
+
 const MainHeader = () => {
+  const { pathname } = useLocation();
+
   return (
-    <header>
-      <div>Boards</div>
-      <div>Account</div>
+    <header className={styles["header"]}>
+      <NavLink className={getStyle} to="/">
+        Boards
+      </NavLink>
+      <NavLink className={getStyleAuth(pathname)} to={"sign-in"}>
+        Account
+      </NavLink>
     </header>
   );
 };

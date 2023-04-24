21c78f7f2d18e536585e2450ecae07b841b556cf
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-04-24 13:44:33 +0300

    add board page (default)

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import "./App.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import RootLayout from "./pages/RootLayout/RootLayout";
+import BoardPage from "./pages/BoardPage";
 
-const router = createBrowserRouter([{ path: "/", element: <RootLayout /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{ index: true, element: <BoardPage /> }],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+import styles from "./Board.module.scss";
+
+interface dataI {
+  title: string;
+  items: string[];
+  // [key:string]:any
+}
+
+interface drag {
+  (
+    e: React.DragEvent<HTMLDivElement>,
+    groupIndex: number,
+    groupItemIndex: number
+  ): void;
+}
+interface dragItem {
+  groupIndex: number;
+  groupItemIndex: number;
+}
+
+const Board = () => {
+  const data: dataI[] = [
+    { title: "group 1", items: ["1", "2", "3"] },
+    { title: "group 2", items: ["4", "5", "6"] },
+  ];
+
+  const [list, setList] = React.useState(data);
+  const [dragging, setDragging] = React.useState(false);
+
+  const dragItem = React.useRef<dragItem | null>();
+  const dragNode = React.useRef<any>();
+
+  const handleDragStart: drag = (e, groupIndex, groupItemIndex) => {
+    dragItem.current = { groupIndex, groupItemIndex };
+    dragNode.current = e.target;
+    dragNode.current.addEventListener("dragend", handleDragEnd);
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+
+  function handleDragEnd() {
+    setDragging(false);
+    dragNode.current.removeEventListener("dragend", handleDragEnd);
+    dragItem.current = null;
+    dragNode.current = null;
+  }
+
+  const handleDragEnter: drag = (e, groupIndex, groupItemIndex) => {
+    const currentItem = dragItem.current;
+    if (
+      groupItemIndex !== dragItem.current?.groupItemIndex ||
+      groupIndex !== dragItem.current?.groupIndex
+    ) {
+      setList((oldList) => {
+        let newList: dataI[] = JSON.parse(JSON.stringify(oldList));
+        if (currentItem) {
+          newList[groupIndex].items.splice(
+            groupItemIndex,
+            0,
+            newList[currentItem.groupIndex].items.splice(
+              currentItem.groupItemIndex,
+              1
+            )[0]
+          );
+        }
+        dragItem.current = { groupIndex, groupItemIndex };
+        return newList;
+      });
+    }
+  };
+
+  function getStyles(groupIndex: number, groupItemIndex: number) {
+    const currentItem = dragItem.current;
+    if (
+      currentItem?.groupIndex === groupIndex &&
+      currentItem.groupItemIndex === groupItemIndex
+    ) {
+      return `${styles["board__group__item--dragging"]} ${
+        styles[`board__group__item`]
+      }`;
+    } else {
+      return styles[`board__group__item`];
+    }
+  }
+
+  return (
+    <div className={styles["board"]}>
+      {list.map((group, groupIndex) => (
+        <div
+          onDragEnter={
+            dragging && !group.items.length
+              ? (e) => {
+                  handleDragEnter(e, groupIndex, 0);
+                }
+              : undefined
+          }
+          onDragEnd={() => {
+            setDragging(false);
+          }}
+          key={groupIndex}
+          className={styles["board__group"]}
+        >
+          <div className={styles["board__group__title"]}>{group.title}</div>
+          {group.items.map((groupItem, groupItemIndex) => (
+            <div
+              onDragStart={(e) => {
+                handleDragStart(e, groupIndex, groupItemIndex);
+              }}
+              onDragEnter={(e) => {
+                handleDragEnter(e, groupIndex, groupItemIndex);
+              }}
+              key={groupItemIndex}
+              draggable
+              className={
+                dragging
+                  ? getStyles(groupIndex, groupItemIndex)
+                  : styles[`board__group__item`]
+              }
+            >
+              {groupItem}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Board;

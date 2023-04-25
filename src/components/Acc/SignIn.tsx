7a8cc8b650b36724280e42584cc4a430a4c7bb05
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-04-25 14:37:48 +0300

    add functionality to sign in

--- src/components/Acc/SignIn.tsx
+++ src/components/Acc/SignIn.tsx
@@ -3,17 +3,51 @@ import styles from "./Form.module.scss";
 
 import Form from "./Form";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "@firebase/auth";
+import { useAppDispatch } from "../../hooks/redux";
+import { setUser } from "../../store/slices/user-slice";
+import { get, getDatabase, ref } from "@firebase/database";
+import { app } from "../../firebase";
 
 const SignIn = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const getUserData = async (
     email: string,
     password: string,
     uName: string
   ) => {
-    console.log(email);
-    console.log(password);
-    console.log(uName);
+    const auth = getAuth();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        const db = getDatabase(app);
+        const userRef = ref(db, `users/${user.uid}`);
+        get(userRef).then((snapshot) => {
+          if (snapshot.exists()) {
+            const reternedData = snapshot.val();
+            if (reternedData.uName === uName) {
+              dispatch(
+                setUser({
+                  email: email,
+                  uName: uName,
+                  password: password,
+                  id: user.uid,
+                })
+              );
+              navigate("/");
+            } else {
+              console.log("Incorrect username");
+            }
+          } else {
+            console.log("no data");
+          }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div>

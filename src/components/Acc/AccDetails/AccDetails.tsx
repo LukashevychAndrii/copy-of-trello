1402f821cf8138982d8cd46844bcb319b380e26e
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-04-25 17:44:19 +0300

    add functionality to sign out

--- src/components/Acc/AccDetails/AccDetails.tsx
+++ src/components/Acc/AccDetails/AccDetails.tsx
@@ -1,11 +1,41 @@
 import React from "react";
 import styles from "./AccDetails.module.scss";
-import { useAppSelector } from "../../../hooks/redux";
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { getAuth, signOut } from "firebase/auth";
+import { createAlert } from "../../../store/slices/alert-slice";
+import { useNavigate } from "react-router";
 
 const AccDetails = () => {
   const userName = useAppSelector((state) => state.user.uName);
   const password = useAppSelector((state) => state.user.password);
   const userEmail = useAppSelector((state) => state.user.email);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  function handleSignOutCLick() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        dispatch(
+          createAlert({
+            alertTitle: "Success!",
+            alertText: "You successfully sign out!",
+            alertError: false,
+          })
+        );
+        navigate("/");
+      })
+      .catch((error) => {
+        dispatch(
+          createAlert({
+            alertTitle: "Error!",
+            alertText: "Sign out failed, database error",
+            alertError: true,
+          })
+        );
+      });
+  }
 
   return (
     <div className={styles["acc-details"]}>
@@ -15,6 +45,7 @@ const AccDetails = () => {
       <div> {password}</div>
       <span>Email:</span>
       <div>{userEmail}</div>
+      <button onClick={handleSignOutCLick}>Sign Out</button>
     </div>
   );
 };

aae9b947a318164f887557db0b267b0aeb04662f
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-05-10 11:59:15 +0300

    remove ticker

--- src/pages/RootLayout/RootLayout.tsx
+++ src/pages/RootLayout/RootLayout.tsx
@@ -3,17 +3,14 @@ import { Outlet } from "react-router-dom";
 
 import MainHeader from "../../components/Header/MainHeader";
 import AlertWindow from "../../utils/AlertWindow/AlertWindow";
-import Ticker from "../../components/Header/Ticker/Ticker";
 
 import { useAppSelector } from "../../hooks/redux";
 
 const RootLayout = () => {
   const theme = useAppSelector((state) => state.theme.theme);
-  const userID = useAppSelector((state) => state.user.id);
   return (
     <>
       <AlertWindow />
-      {!userID && <Ticker />}
       <MainHeader />
       <main data-theme={theme} style={{ minHeight: "80vh" }}>
         <Outlet />
